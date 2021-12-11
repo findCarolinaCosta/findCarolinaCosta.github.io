de0d5e6f0c8f782c7a013fcc0c1b0f14020aceeb
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-12-11 20:23:48 -0300

    Adiciona lógica para o menu aparecer ou não Adicionando ou removendo uma classe ao click em botões para mostrar ou não o menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,35 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close'),
+      navLink = document.querySelectorAll('.nav__link');
+
 
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  });
+}
 
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  });
+}
 
 /*==================== REMOVE MENU MOBILE ====================*/
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu');
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu');
+}
+navLink.forEach(link => link.addEventListener('click', linkAction));
 
 
 /*==================== ACCORDION SKILLS ====================*/
