b033b2abb2e9527b828375c3af8862bd8452bce4
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-12-15 21:15:01 -0300

    Adiciona lógica de alternar o abrir ou fechar da habilidade na seção de habilidade

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,6 +33,22 @@ navLink.forEach(link => link.addEventListener('click', linkAction));
 
 
 /*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills__content'),
+      skillsHeader = document.querySelectorAll('.skills__header');
+
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+
+  for (i = 0; i < skillsContent.length; i += 1 ) {
+    skillsContent[i].className = 'skills__content skills__close';
+  }
+  if (itemClass === 'skills__content skills__close') {
+    this.parentNode.className= 'skills__content skills__open';
+  }
+}
+skillsHeader.forEach((el) => {
+  el.addEventListener('click', toggleSkills);
+})
 
 
 /*==================== QUALIFICATION TABS ====================*/
