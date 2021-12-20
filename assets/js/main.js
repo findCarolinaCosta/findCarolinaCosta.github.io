d59c75bfc89383a9479dc07949492221508d27a0
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-12-20 08:23:51 -0300

    Adiciona refatoração do auto save do vs code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,78 +1,72 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close'),
-      navLink = document.querySelectorAll('.nav__link');
-
-
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close"),
+  navLink = document.querySelectorAll(".nav__link");
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
 if (navToggle) {
-  navToggle.addEventListener('click', () => {
-    navMenu.classList.add('show-menu');
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
   });
 }
-
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
 if (navClose) {
-  navClose.addEventListener('click', () => {
-    navMenu.classList.remove('show-menu');
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
   });
 }
 
 /*==================== REMOVE MENU MOBILE ====================*/
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu');
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu');
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
 }
-navLink.forEach(link => link.addEventListener('click', linkAction));
-
+navLink.forEach((link) => link.addEventListener("click", linkAction));
 
 /*==================== ACCORDION SKILLS ====================*/
-const skillsContent = document.getElementsByClassName('skills__content'),
-      skillsHeader = document.querySelectorAll('.skills__header');
+const skillsContent = document.getElementsByClassName("skills__content"),
+  skillsHeader = document.querySelectorAll(".skills__header");
 
 function toggleSkills() {
   let itemClass = this.parentNode.className;
 
-  for (i = 0; i < skillsContent.length; i += 1 ) {
-    skillsContent[i].className = 'skills__content skills__close';
+  for (i = 0; i < skillsContent.length; i += 1) {
+    skillsContent[i].className = "skills__content skills__close";
   }
-  if (itemClass === 'skills__content skills__close') {
-    this.parentNode.className= 'skills__content skills__open';
+  if (itemClass === "skills__content skills__close") {
+    this.parentNode.className = "skills__content skills__open";
   }
 }
 skillsHeader.forEach((el) => {
-  el.addEventListener('click', toggleSkills);
-})
-
+  el.addEventListener("click", toggleSkills);
+});
 
 /*==================== QUALIFICATION TABS ====================*/
 
-
 /*==================== SERVICES MODAL ====================*/
-const modalViews = document.querySelectorAll('.services__modal'),
-      modalBtns = document.querySelectorAll('.services__button'),
-      modalCloses = document.querySelectorAll('.services__modal-close');
+const modalViews = document.querySelectorAll(".services__modal"),
+  modalBtns = document.querySelectorAll(".services__button"),
+  modalCloses = document.querySelectorAll(".services__modal-close");
 
 let modal = (modalClick) => {
-  modalViews[modalClick].classList.add('active-modal');
-}
+  modalViews[modalClick].classList.add("active-modal");
+};
 
 modalBtns.forEach((modalBtn, i) => {
-  modalBtn.addEventListener('click', () => {
+  modalBtn.addEventListener("click", () => {
     modal(i);
   });
 });
 
 modalCloses.forEach((modalClose) => {
-  modalClose.addEventListener('click', () => {
+  modalClose.addEventListener("click", () => {
     modalViews.forEach((modalView) => {
-      modalView.classList.remove('active-modal');
+      modalView.classList.remove("active-modal");
     });
   });
 });
@@ -94,14 +88,10 @@ let swiper = new Swiper(".portfolio__container", {
 
 /*==================== TESTIMONIAL ====================*/
 
-
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
+/*==================== CHANGE BACKGROUND HEADER ====================*/
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+/*==================== SHOW SCROLL UP ====================*/
 
-
-/*==================== SHOW SCROLL UP ====================*/ 
-
-
-/*==================== DARK LIGHT THEME ====================*/ 
+/*==================== DARK LIGHT THEME ====================*/
