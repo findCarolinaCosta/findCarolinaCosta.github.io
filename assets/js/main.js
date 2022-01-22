fd3d9d22582680621183541f0c7cd34835d21a0d
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-01-22 17:56:48 -0300

    feat: Add scroll-header class to the header tag when scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -113,6 +113,12 @@ function scrollActive() {
 window.addEventListener("scroll", scrollActive);
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader(){
+  const header = document.getElementById('header')
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  if(this.scrollY >= 80) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*==================== SHOW SCROLL UP ====================*/
 
