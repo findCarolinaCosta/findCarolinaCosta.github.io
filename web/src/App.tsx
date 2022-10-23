11d02365b7b4e4b9349ccfc677ed64ebf42731c7
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-10-23 19:10:09 -0300

    feat: home

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,13 @@
 import { Header } from "./components/Header";
+import { Home } from "./components/Home";
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <Home />
+      </main>
     </>
   );
 }
