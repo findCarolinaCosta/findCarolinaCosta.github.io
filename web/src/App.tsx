fd185697dc256aa19770ef456517cbd37a56a41b
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-10-23 19:13:56 -0300

    feat: section about me

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,4 @@
+import { About } from "./components/About";
 import { Header } from "./components/Header";
 import { Home } from "./components/Home";
 
@@ -7,6 +8,7 @@ function App() {
       <Header />
       <main>
         <Home />
+        <About />
       </main>
     </>
   );
