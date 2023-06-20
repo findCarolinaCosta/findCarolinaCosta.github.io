3e1b044d691a7e978cdee30e03bca21049e998df
findCarolinaCosta <carolinadacosta1997@gmail.com>
2023-06-20 12:22:12 -0300

    Revert "refac: change variable declaration to standard sass form"
    This reverts commit 09f65379b98092f1c8c9126ac28e2529a65f8b81.

--- web/src/views/Forbidden.tsx
+++ web/src/views/Forbidden.tsx
@@ -22,7 +22,7 @@ export function Forbidden() {
       <StarBackgroundAnimation />
       <div className="flex flex-col items-center justify-center h-full gap-1">
         <h1 className="text-3xl font-semibold">Error: 403 Forbidden</h1>
-        <span className="text-9xl text-first-color-second font-extrabold sm:text-[16rem]">
+        <span className="text-9xl text-[var(--first-color-second)] font-extrabold sm:text-[16rem]">
           4<i className="uil uil-ban" />3
         </span>
         <p className="text-xl">You don't have access to this page.</p>
