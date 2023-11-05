7fe54c92f1a096c607393e1b9ff3588200204b6f
findCarolinaCosta <carolinadacosta1997@gmail.com>
2023-11-05 18:10:58 -0300

    fix: header layout in medium and large screens

--- apps/web/src/components/contents/Language.tsx
+++ apps/web/src/components/contents/Language.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export function Language({ ...props }) {
   return (
     <div {...props}>
-      <div className="flex space-x-2.5 mx-2 mt-5 md:mt-0 md">
+      <div className="flex space-x-2.5 mx-2 mt-5 md:mt-0 md:flex-col md:space-x-0 md:mx-0 lg:space-x-2.5 lg:flex-row lg:mx-0">
         <Link href="/" className="text-lg">
           🇺🇸
         </Link>
