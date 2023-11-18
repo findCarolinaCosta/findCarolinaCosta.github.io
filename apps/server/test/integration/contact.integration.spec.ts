968674a377544fe5934fb19c67c9e999fc6dddb0
findCarolinaCosta <carolinadacosta1997@gmail.com>
2023-11-18 10:45:04 -0300

    feat: contact endpoint

---
+++ apps/server/test/integration/contact.integration.spec.ts
@@ -0,0 +1,86 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { ContactDTO } from '../../src/dto/contact.dto';
+import { ContactModule } from '../../src/modules/contact/contact.module';
+import { ContactService } from '../../src/modules/contact/contact.service';
+import MockNotionServiceMock from '../mock/MockNotionService.mock';
+
+describe('Contact me endpoint (Integration)', () => {
+  let app: INestApplication;
+  const MockContactService = new ContactService(MockNotionServiceMock);
+  let contactDTO: ContactDTO;
+
+  beforeAll(async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      imports: [ContactModule],
+    })
+      .overrideProvider(ContactService)
+      .useValue(MockContactService)
+      .compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+
+  it('Success in contacting', () => {
+    contactDTO = {
+      Email: 'test@email.com',
+      Message: 'Subject',
+      Name: 'Name Surname',
+      Project: 'Project Name',
+    };
+
+    return request(app.getHttpServer())
+      .post('/contact')
+      .send(contactDTO)
+      .set('Accept', 'application/json')
+      .expect(201);
+  });
+
+  it('Fail in contacting if pass invalid email', () => {
+    contactDTO = {
+      Email: 'test',
+      Message: 'Subject',
+      Name: 'Name Surname',
+      Project: 'Project Name',
+    };
+
+    return request(app.getHttpServer())
+      .post('/contact')
+      .send(contactDTO)
+      .set('Accept', 'application/json')
+      .expect(400)
+      .expect('Content-Type', /json/)
+      .expect({
+        message: ['Email must be an email'],
+        error: 'Bad Request',
+        statusCode: 400,
+      });
+  });
+
+  it('Should return error if Notion service fails', () => {
+    const error = new Error('Notion service error');
+    jest.spyOn(MockNotionServiceMock, 'create').mockImplementationOnce(() => {
+      throw error;
+    });
+
+    contactDTO = {
+      Email: 'test@email.com',
+      Message: 'Subject',
+      Name: 'Name Surname',
+      Project: 'Project Name',
+    };
+
+    return request(app.getHttpServer())
+      .post('/contact')
+      .send(contactDTO)
+      .set('Accept', 'application/json')
+      .expect(500)
+      .expect('Content-Type', /json/)
+      .expect({
+        message: 'Internal server error',
+        statusCode: 500,
+      });
+  });
+});
